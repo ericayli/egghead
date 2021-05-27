73096020c2b64fb46d7c006d2a0d7f1aaedd2e09
oserpent <57163961+oserpent@users.noreply.github.com>
2021-05-26 23:29:33 -0700

    Update brainflow_gui.py

--- brainflow_gui.py
+++ brainflow_gui.py
@@ -29,7 +29,7 @@ mac_address = '12345'
 colsused = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16])
 
 # plot settings
-display_interval = 1000
+display_interval = 5000
 leftbord = nan
 rightbord = nan
 welch_left = nan
@@ -40,8 +40,7 @@ fft_type = "welch"
 sampling_rate = BoardShim.get_sampling_rate(board_id)
 
 # welch settings
-welch_all = True
-per_data_seg = 0.2
+per_data_seg = 1
 poverlap = 0.5
 
 # bandpass settings
@@ -64,148 +63,113 @@ avg_thres = 0.2
 min_dis = 1
 avg_min_dis = 1
 
+def bandpass_data(data, left_border, right_border):
+    if (apply_bandpass):
+        sos = sig.butter(3, [bandpass_left*2/sampling_rate, bandpass_right*2/sampling_rate], btype = 'bandpass', output = 'sos')
+        b_data = sig.sosfilt(sos, b_data)[left_border:right_border]
+    else:
+        b_data = data[left_border:right_border]
+    return b_data
+
+def process_data(data):
+    if (fft_type == "welch"):
+        p_freq, p_data = signal.welch(data, fs = sampling_rate, nperseg = per_data_seg * data.size, noverlap = poverlap * per_data_seg * data.size)
+    else:
+        p_data = np.abs(np.fft.fft(data))
+        p_freq = np.array(np.fft.fftfreq(len(p_data), 1/sampling_rate))
+        p_freq = np.array([x for x in p_freq if x >= 0])
+        p_data = p_data[:p_freq.size]
+    return p_freq, p_data
+
+def find_lims(freq, data, left_index, right_index):
+    left_lim = 0
+    right_lim = data.size
+    if (not math.isnan(left_index)):
+        left_lim = math.ceil(np.where(freq > left_index)[0][0])
+    if (not math.isnan(right_index)):
+        right_lim = math.ceil(np.where(freq > right_index)[0][0])
+    return left_lim, right_lim
+
 def animate(i, board):
 
     data = board.get_board_data().T
     DataFilter.write_file(data.T, output_file, 'a') if create_csv else True
+
     data = data[:, colsused] if isinstance(colsused, np.ndarray) else data[:, :]
-    cols = np.arange(data[0].size) if not isinstance(colsused, np.ndarray) else colsused
+    cols = colsused if isinstance(colsused, np.ndarray) else np.arange(data[0].size)
     maxdat = data[:, 0].size if len(data.shape) > 1 else data.size
     leftborder = 0 if math.isnan(leftbord) else leftbord
     rightborder = maxdat if math.isnan(rightbord) else rightbord
-
-    # inefficient vars
-    max_welch = 0
-    avg_freq = np.zeros(maxdat)
     sum_welch = np.zeros(maxdat)
-    welch_size = 0
 
-    ax1 = plt.subplot(3 if avg_plot and data[0].size > 1 else 2, 1, 1)
+    # time domain plot
+    ax1 = plt.subplot(3 if avg_plot and cols.size > 1 else 2, 1, 1)
     plt.cla()
     ax1.set_title("Time Domain")
     ax1.set_xlabel("Sample Index")
     ax1.set_ylabel("Voltage (mV)")
-    for i in range (data[0].size):
-        if (apply_bandpass):
-            if (len(data.shape) > 1):
-                newdat = data[:, i]
-            else:
-                newdat = data
-
-            sos = sig.butter(3, [bandpass_left*2/sampling_rate, bandpass_right*2/sampling_rate], btype = 'bandpass', output = 'sos')
-            newdat = sig.sosfilt(sos, newdat)[leftborder:rightborder]
-        else:
-            if (len(data.shape) > 1):
-                newdat = data[leftborder:rightborder, i]
-            else:
-                newdat = data[leftborder:rightborder]
+    for i in range (cols.size):
+        newdat = bandpass_data(data[:, i] if len(data.shape) > 1 else data, leftborder, rightborder)
         index = leftborder + np.arange(newdat.size)
         ax1.plot(index, newdat)
-        if (legend and data[0].size > 1):
+        if (legend and cols.size > 1):
             if ((cols.size < 12 and avg_plot == False) or (cols.size < 6 and avg_plot == True)):
                 ax1.legend(cols)
 
-    ax2 = plt.subplot(3 if avg_plot and data[0].size > 1 else 2, 1, 2)
+    # fft/welch plot for all channels
+    ax2 = plt.subplot(3 if avg_plot and cols.size > 1 else 2, 1, 2)
     plt.cla()
     ax2.set_title("Frequency Domain using " + fft_type)
     ax2.set_xlabel("Frequency (Hz)")
     ax2.set_ylabel("Amplitude")
-    for i in range (data[0].size):
-        if (apply_bandpass):
-            if (len(data.shape) > 1):
-                newdat = data[:, i]
-            else:
-                newdat = data
-            sos = sig.butter(3, [bandpass_left*2/sampling_rate, bandpass_right*2/sampling_rate], btype = 'bandpass', output = 'sos')
-            newdat = sig.sosfilt(sos, newdat)[leftborder:rightborder]
-        else:
-            if (len(data.shape) > 1):
-                newdat = data[leftborder:rightborder, i]
-            else:
-                newdat = data[leftborder:rightborder]
-
-        if (fft_type == "welch"):
-            welchfreq, welch_data = signal.welch(newdat, fs = sampling_rate, nperseg = per_data_seg * data[:, 0].size if not welch_all else newdat.size, noverlap = poverlap * (per_data_seg * data[:, 0].size if not welch_all else newdat.size))
-        else:
-            welch_data = np.abs(np.fft.fft(newdat))
-            welchfreq = np.fft.fftfreq(len(welch_data), 1/sampling_rate)
-            welchfreq = np.array([x for x in welchfreq if x >= 0])
-            welch_data = welch_data[:welchfreq.size]
-
-        welch_size = welch_data.size
-        avg_freq = welchfreq
-        left_lim = 0
-        right_lim = welch_data.size
-        if (not math.isnan(welch_left)):
-            left_lim = math.ceil(np.where(welchfreq > welch_left)[0][0])
-        if (not math.isnan(welch_right)):
-            right_lim = math.ceil(np.where(welchfreq > welch_right)[0][0])
-        if (max(welch_data[left_lim:right_lim]) > max_welch):
+    # plot
+    for i in range (cols.size):
+        newdat = bandpass_data(data[:, i] if len(data.shape) > 1 else data, leftborder, rightborder)
+        welchfreq, welch_data = process_data(newdat)
+        if 'left-lim' and 'right_lim' not in locals():
+            left_lim, right_lim = find_lims(welchfreq, welch_data, welch_left, welch_right)
+        if 'avg_freq' not in locals():
+            avg_freq = welchfreq
+        if 'max_welch' not in locals():
+            max_welch = welch_data[left_lim]
+        elif max(welch_data[left_lim:right_lim]) > max_welch:
             max_welch = max(welch_data[left_lim:right_lim])
         ax2.plot(welchfreq[left_lim:right_lim], welch_data[left_lim:right_lim])
-        if (legend and data[0].size > 1):
-            if ((cols.size < 12 and avg_plot == False) or (cols.size < 6 and avg_plot== True)):
+        if (legend and cols.size > 1):
+            if ((cols.size < 12 and not avg_plot) or (cols.size < 6 and avg_plot)):
                 ax2.legend(cols)
             else:
                 max_y = max(welch_data[left_lim:right_lim])
                 max_x = welchfreq[welch_data[left_lim:right_lim].argmax()]
                 ax2.annotate(cols[i], (max_x, max_y), color = "red", verticalalignment = "bottom", weight = "bold")
+    # plotting peaks
+    for i in range (cols.size):
+        newdat = bandpass_data(data[:, i] if len(data.shape) > 1 else data, leftborder, rightborder)
+        welchfreq, welch_data = process_data(newdat)
 
-    sum_welch = np.zeros(welch_size)
-
-    for i in range (data[0].size):
-        #inefficient code
-        if (apply_bandpass):
-            if (len(data.shape) > 1):
-                newdat = data[:, i]
-            else:
-                newdat = data
-            sos = sig.butter(3, [bandpass_left*2/sampling_rate, bandpass_right*2/sampling_rate], btype = 'bandpass', output = 'sos')
-            newdat = sig.sosfilt(sos, newdat)[leftborder:rightborder]
-        else:
-            if (len(data.shape) > 1):
-                newdat = data[leftborder:rightborder, i]
-            else:
-                newdat = data[leftborder:rightborder]
-
-        if (fft_type == "welch"):
-            welchfreq, welch_data = signal.welch(newdat, fs = sampling_rate, nperseg = per_data_seg * data[:, 0].size if not welch_all else newdat.size, noverlap = poverlap * (per_data_seg * data[:, 0].size if not welch_all else newdat.size))
-        else:
-            welch_data = np.abs(np.fft.fft(newdat))
-            welchfreq = np.fft.fftfreq(len(welch_data), 1/sampling_rate)
-            welchfreq = np.array([x for x in welchfreq if x >= 0])
-            welch_data = welch_data[:welchfreq.size]
-
-        for j in range (welch_size):
+        for j in range (welch_data.size):
             sum_welch[j] = sum_welch[j] + welch_data[j]
 
-        left_lim = 0
-        right_lim = welch_data.size
-        if (not math.isnan(welch_left)):
-            left_lim = math.ceil(np.where(welchfreq > welch_left)[0][0])
-        if (not math.isnan(welch_right)):
-            right_lim = math.ceil(np.where(welchfreq > welch_right)[0][0])
-
         if (label):
-            #inefficient code
             index = peakutils.indexes(welch_data[left_lim:right_lim], thres=0, min_dist = math.ceil(np.where(welchfreq > 2 + min_dis)[0][0] - math.ceil(np.where(welchfreq > 2)[0][0])))
             index = [x for x in index if welch_data[left_lim:right_lim][x] > label_thres * max_welch]
             for j, value in enumerate(index):
                 ax2.plot(welchfreq[left_lim:right_lim][value], welch_data[left_lim:right_lim][value], marker="o", ls="", ms=3)
                 ax2.annotate("{:.2f}".format(welchfreq[left_lim:right_lim][value]), (welchfreq[left_lim:right_lim][value], welch_data[left_lim:right_lim][value]), verticalalignment = "top")
 
-    if (avg_plot and data[0].size > 1):
+    # average fft/welch plot
+    if (avg_plot and cols.size > 1):
         ax3 = plt.subplot(313)
         plt.cla()
         ax3.set_title("Average Frequency Domain using " + fft_type)
         ax3.set_xlabel("Frequency (Hz)")
         ax3.set_ylabel("Amplitude")
-        ax3.plot(avg_freq[left_lim:right_lim], sum_welch[left_lim:right_lim]/data[0].size)
-        index = peakutils.indexes(sum_welch[left_lim:right_lim]/data[0].size, thres = avg_thres, min_dist = math.ceil(np.where(welchfreq > 2 + avg_min_dis)[0][0] - math.ceil(np.where(welchfreq > 2)[0][0])))
+        ax3.plot(avg_freq[left_lim:right_lim], sum_welch[left_lim:right_lim]/cols.size)
+        index = peakutils.indexes(sum_welch[left_lim:right_lim]/cols.size, thres = avg_thres, min_dist = math.ceil(np.where(welchfreq > 2 + avg_min_dis)[0][0] - math.ceil(np.where(welchfreq > 2)[0][0])))
         if (label):
             for j, value in enumerate(index):
-                ax3.plot(avg_freq[left_lim:right_lim][value], sum_welch[left_lim:right_lim][value]/data[0].size, marker="o", ls="", ms=3)
-                ax3.annotate("{:.2f}".format(avg_freq[left_lim:right_lim][value]), (avg_freq[left_lim:right_lim][value], sum_welch[left_lim:right_lim][value]/data[0].size), verticalalignment = "top")
+                ax3.plot(avg_freq[left_lim:right_lim][value], sum_welch[left_lim:right_lim][value]/cols.size, marker="o", ls="", ms=3)
+                ax3.annotate("{:.2f}".format(avg_freq[left_lim:right_lim][value]), (avg_freq[left_lim:right_lim][value], sum_welch[left_lim:right_lim][value]/cols.size), verticalalignment = "top")
 
     plt.tight_layout()
 
