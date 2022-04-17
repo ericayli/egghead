628784b34acb1cd7572788381535973539eecfaf
Orrin Zhong <57163961+oserpent@users.noreply.github.com>
2022-04-16 17:56:23 -0700

    Update prompt.py
    Added clench and raise csv edits

--- prompt.py
+++ prompt.py
@@ -45,6 +45,12 @@ clench_img = pygame.transform.scale(clench_img, (clench_img.get_width()//3, clen
 raise_img = pygame.image.load(os.path.join('Assets', 'raise.png'))
 raise_img = pygame.transform.scale(raise_img, (raise_img.get_width()//3, raise_img.get_height()//3))
 
+def label_extractor(input, id, array):
+    try:
+        return input(id) if array else [input(id)]
+    except:
+        return []
+
 def draw_window():
     WIN.fill(WHITE)
     WIN.blit(three_img, (300, 100))
@@ -53,7 +59,7 @@ def draw_window():
 def clench_prompt(counter):
     event_img = clench_img
     screen_col = WHITE
-    space_prompt = False
+    raise_indicate, clench_indicate = False, False
     if counter == 3:
         show_img = three_img
     if counter == 2:
@@ -63,13 +69,13 @@ def clench_prompt(counter):
     if counter == 0:
         show_img = None
         screen_col = RED
-        space_prompt = True
-    return show_img, screen_col, event_img, space_prompt
+        clench_indicate = True
+    return show_img, screen_col, event_img, raise_indicate, clench_indicate
 
 def raise_prompt(counter):
     event_img = raise_img
     screen_col = WHITE
-    space_prompt = False
+    raise_indicate, clench_indicate = False, False
     if counter == 3:
         show_img = three_img
     if counter == 2:
@@ -79,8 +85,8 @@ def raise_prompt(counter):
     if counter == 0:
         show_img = None
         screen_col = RED
-        space_prompt = True
-    return show_img, screen_col, event_img, space_prompt
+        raise_indicate = True
+    return show_img, screen_col, event_img, raise_indicate, clench_indicate
 
 def random_prompt():
     event_int = random.randint(0, 1);
@@ -93,15 +99,15 @@ def main():
     show_img = None
     event_img = None
     screen_col = None
-    #clock = pygame.time.Clock()
     counter = 3
+    #number of trials
     trials = 3
     event_function = random_prompt()
     pygame.time.set_timer(pygame.USEREVENT, 1000)
     run = True
     print_first = True
-    space_pressed = False
-    space_prompt = False
+    raise_indicate = False
+    clench_indicate = False
 
     BoardShim.enable_dev_board_logger()
     params = BrainFlowInputParams()
@@ -115,35 +121,58 @@ def main():
     board.prepare_session()
     board.start_stream()
     BoardShim.log_message(LogLevels.LEVEL_INFO.value, 'start sleeping in the main thread')
+
+    label_list = [None] * BoardShim.get_num_rows(board_id)
+    label_dict = {
+        "Battery" : label_extractor(BoardShim.get_battery_channel, board_id, False),
+        "EEG" : label_extractor(BoardShim.get_eeg_channels, board_id, True),
+        "EMG" : label_extractor(BoardShim.get_emg_channels, board_id, True),
+        "ECG" : label_extractor(BoardShim.get_ecg_channels, board_id, True),
+        "Temperature" : label_extractor(BoardShim.get_temperature_channels, board_id, True),
+        "Resistance" : label_extractor(BoardShim.get_resistance_channels, board_id, True),
+        "EOG" : label_extractor(BoardShim.get_eog_channels, board_id, True),
+        "EXG" : label_extractor(BoardShim.get_exg_channels, board_id, True),
+        "EDA" : label_extractor(BoardShim.get_eda_channels, board_id, True),
+        "PPG" : label_extractor(BoardShim.get_ppg_channels, board_id, True),
+        "Accel" : label_extractor(BoardShim.get_accel_channels, board_id, True),
+        "Analog" : label_extractor(BoardShim.get_analog_channels, board_id, True),
+        "Gyro" : label_extractor(BoardShim.get_gyro_channels, board_id, True),
+        "Other" : label_extractor(BoardShim.get_other_channels, board_id, True)
+    }
+
+    for key, value in label_dict.items():
+        for count, channel in enumerate(value):
+            label_list[channel] = key +  " " + str(count) if len(value) > 1 else key
+
     with open(output_file, 'w', newline = '') as file:
         writer = csv.writer(file)
-        writer.writerow(["Sampling Rate: " + str(sampling_rate)])
-    #time.sleep(1)
+        #writer.writerow(np.arange(BoardShim.get_num_rows(board_id)))
+        writer.writerow(["Sampling Rate: " + str(BoardShim.get_sampling_rate(board_id)) + " Hz"])
+        writer.writerow(["Board: " + BoardShim.get_device_name(board_id)])
+        writer.writerow(np.append(np.append(label_list, "Raise"), "Clench"))
 
     while run:
         data = board.get_board_data().T
-
-        keys_pressed = pygame.key.get_pressed()
-        if keys_pressed[pygame.K_SPACE]:
-            space_pressed = True
-        else:
-            space_pressed = False
         
         with open(output_file, 'a', newline = '') as file:
             writer = csv.writer(file)
-            if (print_first):
-                writer.writerow(np.arange(data[0].size))
-                print_first = False
+            # if (print_first):
+            #     writer.writerow(np.arange(data[0].size))
+            #     print_first = False
             for i in range(data[:, 0].size):
-                if space_pressed:
-                    space_event = 1
+                if raise_indicate:
+                    raise_event = 1
                 else:
-                    space_event = 0
-                writer.writerow(np.append(np.append(data[i, :], space_prompt), space_event))
-        #clock.tick(FPS)
+                    raise_event = 0
+                if clench_indicate:
+                    clench_event = 1
+                else:
+                    clench_event = 0
+                writer.writerow(np.append(np.append(data[i, :], raise_event), clench_event))
+
         for event in pygame.event.get():
             if event.type == pygame.USEREVENT: 
-                show_img, screen_col, event_img, space_prompt = event_function(counter)
+                show_img, screen_col, event_img, raise_indicate, clench_indicate = event_function(counter)
                 counter -= 1
                 if counter < 0 and trials > 1:
                     trials -= 1
@@ -152,8 +181,6 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        #keys_pressed = pygame.key.get_pressed()
-        #draw_window()
         if screen_col:
             WIN.fill(screen_col)
         if event_img:
